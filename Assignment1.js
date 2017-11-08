a1b2e3ce07be46019486840df352cf358288e666
Nidhi16 <nidhi.valuable@gmail.com>
2017-11-09 00:51:05 +0530

    Search functionality implemented

--- Assignment1.js
+++ Assignment1.js
@@ -20,40 +20,46 @@ function DataStore() {
         delete this.dict[id];
     };
     // Get all the objects
-    this.getItems = function() {
-        // console.log(this.dict);
-        // console.log(Object.keys(this.dict));
-        var items = [];
-        var self = this;
-        Object.keys(this.dict).forEach(function(id) {
-            var temp = {};
-            temp['id'] = id;
-            temp['val'] = self.dict[id];
-            items.unshift(temp);
-        });
-        return items;
+    this.getIds = function() {
+        return Object.keys(this.dict);
+    };
+    this.search = function(query) {
+        var searchResults = [];
+        var ids = this.getIds();
+        ids.forEach(function(id) {
+            var item_text = this.getItem(id)();
+            if (item_text.toLowerCase().indexOf(query.toLowerCase()) != -1) {
+                searchResults.unshift({
+                    'id': id,
+                    'val': item_text
+                });
+            }
+        }, this);
+        return searchResults;
+    };
+    this.getCount = function() {
+        return Object.keys(this.dict).length;
     };
 }
 
 var leftObj = new DataStore();
 var rightObj = new DataStore();
 
-leftObj.setItem(0, 'Adrian Kirkiand');
-leftObj.setItem(1, 'Alfreda Davenport');
-leftObj.setItem(2, 'Avis Hendricks');
-leftObj.setItem(3, 'Beatriz Blackburn');
-leftObj.setItem(4, 'Bray Knight');
-leftObj.setItem(5, 'Callie Price');
-leftObj.setItem(6, 'Casandra Workman');
-leftObj.setItem(7, 'John Doe');
-leftObj.setItem(8, 'Jane Doe');
-leftObj.setItem(9, 'Ethan Bailey');
-leftObj.setItem(10, 'John Snow');
+leftObj.setItem(100, 'Adrian Kirkiand');
+leftObj.setItem(101, 'Alfreda Davenport');
+leftObj.setItem(102, 'Avis Hendricks');
+leftObj.setItem(103, 'Beatriz Blackburn');
+leftObj.setItem(104, 'Bray Knight');
+leftObj.setItem(105, 'Callie Price');
+leftObj.setItem(106, 'Casandra Workman');
+leftObj.setItem(107, 'John Doe');
+leftObj.setItem(108, 'Jane Doe');
+leftObj.setItem(109, 'Ethan Bailey');
+leftObj.setItem(110, 'John Snow');
 
-function populateLeftList() {
-    var result = leftObj.getItems();
+function populateLeftList(result) {
+    // var result = leftObj.getItems();
     // console.log(result);
-    
     var leftPanelSelect = document.getElementById('ava_employee');
     leftPanelSelect.innerText = "";
     result.forEach(function(dict){
@@ -66,8 +72,8 @@ function populateLeftList() {
     });
 }
 
-function populateRightList() {
-    var result = rightObj.getItems();
+function populateRightList(result) {
+    // var result = rightObj.getItems();
 
     var rightPanelSelect = document.getElementById('selec_employee');
     rightPanelSelect.innerText = "";
@@ -81,14 +87,29 @@ function populateRightList() {
     });
 }
 
-populateLeftList();
-populateRightList();
-
 var available_employee_count = document.getElementById('available_employee_count');
 var selected_employee_count = document.getElementById('selected_employee_count');
 
-available_employee_count.innerText = leftObj.getItems().length;
-selected_employee_count.innerText = rightObj.getItems().length;
+available_employee_count.innerText = leftObj.getCount();
+selected_employee_count.innerText = rightObj.getCount();
+
+// Search
+var firstSearchBox = document.getElementById('first');
+var secondSearchBox = document.getElementById('second');
+
+var fillterLeftList = function() {
+    var queryTerm = firstSearchBox.value;
+    var results = leftObj.search(queryTerm);
+    populateLeftList(results);
+};
+firstSearchBox.addEventListener('input', fillterLeftList);
+
+var filterRightList = function() {
+    var queryTerm = secondSearchBox.value;
+    var results = rightObj.search(queryTerm);
+    populateRightList(results);
+};
+secondSearchBox.addEventListener('input', filterRightList);
 
 var single_right_btn = document.getElementById('single_right_btn');
 var multiple_right_btn = document.getElementById('multiple_right_btn');
@@ -98,16 +119,15 @@ var multiple_left_btn = document.getElementById('multiple_left_btn');
 single_right_btn.addEventListener('click', function(){
     var item_id = parseInt(document.getElementById('ava_employee').value);
     // debugger;
-    var item_text = leftObj.getItem(item_id);
+    var item_text = leftObj.getItem(item_id)();
     console.log(item_id);
     // console.log(item_text);
-    rightObj.setItem(item_id, item_text());
+    rightObj.setItem(item_id, item_text);
     leftObj.removeItem(item_id);
-    populateLeftList();
-    populateRightList();
-    available_employee_count.innerText = leftObj.getItems().length;
-    selected_employee_count.innerText = rightObj.getItems().length;
-    
+    fillterLeftList();
+    filterRightList();
+    available_employee_count.innerText = leftObj.getCount();
+    selected_employee_count.innerText = rightObj.getCount();    
 });
 
 multiple_right_btn.addEventListener('click', function(){
@@ -115,34 +135,29 @@ multiple_right_btn.addEventListener('click', function(){
     for (var i = 0; i < elem.options.length; i++) {
         if(elem.options[i].selected == true){
              var item_id = parseInt(elem.options[i].value);
-             console.log(item_id);
-             var item_text = leftObj.getItem(item_id);
-             console.log(item_text());
-             rightObj.setItem(item_id, item_text());
-             console.log(rightObj.getItem(item_id)());
+             var item_text = leftObj.getItem(item_id)();
+             rightObj.setItem(item_id, item_text);
              leftObj.removeItem(item_id);
         }
     }
-    populateLeftList();
-    populateRightList();
-    available_employee_count.innerText = leftObj.getItems().length;
-    selected_employee_count.innerText = rightObj.getItems().length;
-    
+    fillterLeftList();
+    filterRightList();
+    available_employee_count.innerText = leftObj.getCount();
+    selected_employee_count.innerText = rightObj.getCount();   
 });
 
 single_left_btn.addEventListener('click', function(){
     var item_id = parseInt(document.getElementById('selec_employee').value);
     // debugger;
-    var item_text = rightObj.getItem(item_id);
+    var item_text = rightObj.getItem(item_id)();
     console.log(item_id);
     // console.log(item_text);
-    leftObj.setItem(item_id, item_text());
+    leftObj.setItem(item_id, item_text);
     rightObj.removeItem(item_id);
-    populateLeftList();
-    populateRightList();
-    available_employee_count.innerText = leftObj.getItems().length;
-    selected_employee_count.innerText = rightObj.getItems().length;
-    
+    fillterLeftList();
+    filterRightList();
+    available_employee_count.innerText = leftObj.getCount();
+    selected_employee_count.innerText = rightObj.getCount(); 
 });
 
 multiple_left_btn.addEventListener('click', function(){
@@ -150,14 +165,18 @@ multiple_left_btn.addEventListener('click', function(){
     for (var i = 0; i < elem.options.length; i++) {
         if(elem.options[i].selected == true){
              var item_id = parseInt(elem.options[i].value);
-             var item_text = rightObj.getItem(item_id);
-             leftObj.setItem(item_id, item_text());
+             var item_text = rightObj.getItem(item_id)();
+             leftObj.setItem(item_id, item_text);
              rightObj.removeItem(item_id);
         }
     }
-    populateLeftList();
-    populateRightList();
-    available_employee_count.innerText = leftObj.getItems().length;
-    selected_employee_count.innerText = rightObj.getItems().length;
-    
+    fillterLeftList();
+    filterRightList();
+    available_employee_count.innerText = leftObj.getCount();
+    selected_employee_count.innerText = rightObj.getCount();    
 });
+
+
+// populateLeftList();
+fillterLeftList();
+filterRightList();
